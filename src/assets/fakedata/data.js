f3d0514f07a2b0dbf2effcf9f277e07f6b713641
danielbpc2 <danielbpc2@hotmail.com>
2020-10-27 16:57:48 -0300

    Data: new datas

---
+++ src/assets/fakedata/data.js
@@ -0,0 +1,78 @@
+
+  const users = [
+    {
+    "id": 1,
+    "email": "email@email.com",
+    "pasword": 123456
+    }]
+
+  const actors =  [
+    {
+      "id": 1,
+      "name": "string, nome artistico",
+      "age": 18,
+      "sex": "string,Masc, Fem, Outros",
+      "salary": 69.99,
+      "genre": ["string", "drama", "ação"],
+      "avatar": "file",
+      "drt": "0000/UF",
+      "user_id": 1
+    },{
+      "id": 2,
+      "name": "Daniel Carvalho",
+      "age": 27,
+      "sex": "Masculino",
+      "salary": 69.99,
+      "genre": ["drama", "ação"],
+      "avatar": "https://images.unsplash.com/photo-1499996860823-5214fcc65f8f?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+      "drt": "0069/PE",
+      "user_id": 2
+    },{
+      "id": 3,
+      "name": "Deboratriz",
+      "age": 28,
+      "sex": "Feminino",
+      "salary": 69.99,
+      "genre": ["drama", "Comédia"],
+      "avatar": "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
+      "drt": "0420/SP",
+      "user_id": 3
+    },{
+      "id": 4,
+      "name": "Matheus",
+      "age": 26,
+      "sex": "Masculino",
+      "salary": 69.99,
+      "genre": ["ação", "aventura"],
+      "avatar": "file",
+      "drt": "0000/UF",
+      "user_id": 4
+    }
+  ]
+  //produtor
+  const produtors = [
+  {
+    "user_id": 1
+  }
+]
+const disponibilidade =
+[
+  {
+    "start_Date": "date",
+    "end_date": "date",
+    "actor_id": 1,
+    "producer_id": 1
+  },
+  {
+    "start_Date": "date",
+    "end_date": "date",
+    "actor_id": 2,
+    "producer_id": 1
+  },
+  {
+    "start_Date": "date",
+    "end_date": "date",
+    "actor_id": 1,
+    "producer_id": 2
+  }
+]
