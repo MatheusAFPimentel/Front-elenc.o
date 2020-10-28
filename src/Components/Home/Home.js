04b09516ce9a83563ba192f432c3dfadcce666ab
Rodrigo <moura.desenvolvimento@gmail.com>
2020-10-28 15:51:09 -0300

    Atualização layout pagina

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,3 +1,16 @@
 import React from 'react';
+import './home.css'
 
-export default Home;
+export default function Home() {
+    return (
+        <>
+        <h3>Digite o nome do Ator</h3>
+        <input type="text"></input>
+            <ul class="list-group">
+                <li class="list-group-item">First item</li>
+                <li class="list-group-item">Second item</li>
+                <li class="list-group-item">Third item</li>
+            </ul>
+        </>
+    )
+}
