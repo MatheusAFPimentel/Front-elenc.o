646c9878a94ec6ddc725301e260691868cc7a3e9
danielbpc2 <danielbpc2@hotmail.com>
2020-10-29 16:07:13 -0300

    FIX: Sobre

--- src/Pages/Sobre/Sobre.js
+++ src/Pages/Sobre/Sobre.js
@@ -4,21 +4,21 @@ import CardTime from "../CardTime/CardTime";
 import time from "./time";
 
 function Sobre() {
-  time.map((membro, index) => {
-    return (
-      <div>
-        <h3>
-          O Elenc.o foi desenvolvido para a Gama Academy em parceria com
-          Accenture. Nosso time de desenvolvedores junior é composto por:
-        </h3>
-        <div className="container_cards">
+  return (
+    <div>
+      <h3>
+        O Elenc.o foi desenvolvido para a Gama Academy em parceria com
+        Accenture. Nosso time de desenvolvedores junior é composto por:
+      </h3>
+      <div className="container_cards">
+        {time.map((membro, index) => (
           <div key={index}>
             <CardTime membro={membro} />
           </div>
-        </div>
+        ))}
       </div>
-    );
-  });
+    </div>
+  );
 }
 
 export default Sobre;
