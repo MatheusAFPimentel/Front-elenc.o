13761cd5c0fa524d8c28f69343735357cd6ff41c
Rodrigo <moura.desenvolvimento@gmail.com>
2020-10-29 16:24:23 -0300

    atualização no app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import Home from './Components/Home/Home';
+import Cadastro from './pages/cadastro'
 
 
 export default function app() {
   return (
     <>
-  <Home />
+ 
+  <Cadastro />
   </>
   
   )
