0915cbd757d2d97fac4f69e49b2d651fb5ac4004
danielbpc2 <danielbpc2@hotmail.com>
2020-10-29 16:10:11 -0300

    Styling: Css

--- src/Pages/ProducerHome/index.js
+++ src/Pages/ProducerHome/index.js
@@ -1,7 +1,15 @@
+import { Col, Row, Container } from "reactstrap";
 import "./index.css";
 
 const ProducerHome = () => {
-  return <div className="home_producer_background"></div>;
+  return (
+    <div className="home_producer_container">
+      <div className="home_producer_sidebar">
+        <h1>hey</h1>
+      </div>
+      <div className="home_producer_background"></div>
+    </div>
+  );
 };
 
 export default ProducerHome;
