335b709de67418e0a76cd91db5002799cef8c6b0
deboranis <dc.nisen@hotmail.com>
2020-10-29 14:59:02 -0300

    react router dom e imagens

--- src/Components/PainelResultados/PainelResultados.js
+++ src/Components/PainelResultados/PainelResultados.js
@@ -1,3 +1,7 @@
 import React from 'react';
+import './painelResultados.css';
+import { useLocation } from 'react-router-dom';
+import CardAtores from '../CardsAtores/CardsAtores';
+
 
 export default PainelResultados;
