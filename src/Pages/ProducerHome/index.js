45a2434d3463acd048c1344803d1873a29bc721b
danielbpc2 <danielbpc2@hotmail.com>
2020-10-30 13:52:50 -0300

    Basic strucuture

--- src/Pages/ProducerHome/index.js
+++ src/Pages/ProducerHome/index.js
@@ -4,7 +4,29 @@ const ProducerHome = (props) => {
   return (
     <div className="home_producer_container">
       <div className="home_producer_sidebar">
-        <h1>hey</h1>
+        <header>
+          <h2>Buscar</h2>
+        </header>
+        <main>
+          <form action="">
+            <div className="form-group">
+              <label htmlFor="">Quantos atores precisam</label>
+              <input type="text" />
+            </div>
+            <div className="form-group">
+              <label htmlFor="">Genero da obra (Separado por virgulas)</label>
+              <input type="text" />
+            </div>
+            <div className="form-group">
+              <label htmlFor="">Data de inicio</label>
+              <input type="date" />
+            </div>
+            <div className="form-group">
+              <label htmlFor="">Data de fim</label>
+              <input type="date" />
+            </div>
+          </form>
+        </main>
       </div>
       <div className="home_producer_background">
         <h1>Você não fez nenhuma busca ainda</h1>
