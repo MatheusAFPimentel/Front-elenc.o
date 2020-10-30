24320d4d05a3245241a0a94f5e0bf9436e06c345
danielbpc2 <danielbpc2@hotmail.com>
2020-10-30 16:08:25 -0300

    Feat: Added State functionality to component

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,21 +1,25 @@
 import React from "react";
-import './home.css';
-import logo from '../../assets/images/logo.svg';
+import "./home.css";
+import logo from "../../assets/images/logo.svg";
 
 function Home() {
   return (
-    <div className='home'>
-      <img id='logo' src={logo} alt='logo elenc.o'/>
-      <div className='form_login'>
+    <div className="home">
+      <img id="logo" src={logo} alt="logo elenc.o" />
+      <div className="form_login">
         <form>
-          <h2 className='titulo_login'>Login</h2>
-          <label>Email</label> <br/>
-          <input type="email" placeholder="insira seu email" required /> <br/>
-          <label>Senha</label><br/>
-          <input type="password" placeholder="insira sua senha" required /> <br/>
-          <button id='botao_entrar' type="submit">Entrar</button>
+          <h2 className="titulo_login">Login</h2>
+          <label>Email</label> <br />
+          <input type="email" placeholder="insira seu email" required /> <br />
+          <label>Senha</label>
+          <br />
+          <input type="password" placeholder="insira sua senha" required />{" "}
+          <br />
+          <button id="botao_entrar" type="submit">
+            Entrar
+          </button>
         </form>
-        <div className='container_cadastro'>
+        <div className="container_cadastro">
           <a href="" className="link_cadastro">
             Cadastre-se como Produtor
           </a>
@@ -28,3 +32,5 @@ function Home() {
     </div>
   );
 }
+
+export default Home;
