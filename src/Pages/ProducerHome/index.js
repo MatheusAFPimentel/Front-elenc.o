8718b372e86f69e8b02041aa1bd8037208d75678
danielbpc2 <danielbpc2@hotmail.com>
2020-10-30 15:24:27 -0300

    Feat: React-icons

--- src/Pages/ProducerHome/index.js
+++ src/Pages/ProducerHome/index.js
@@ -1,5 +1,5 @@
 import "./index.css";
-
+import { FaSearch } from "react-icons/fa";
 const ProducerHome = (props) => {
   return (
     <div className="home_producer_container">
@@ -11,22 +11,40 @@ const ProducerHome = (props) => {
           <form action="">
             <div className="form-group">
               <label htmlFor="">Quantos atores precisam</label>
-              <input type="text" />
+              <select name="" id="">
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+                <option value="6">6</option>
+                <option value="7">7</option>
+                <option value="8">8</option>
+                <option value="9">9</option>
+                <option value="10">10</option>
+              </select>
             </div>
+
             <div className="form-group">
               <label htmlFor="">Genero da obra (Separado por virgulas)</label>
-              <input type="text" />
+              <input type="text" placeholder="Drama, Ação" />
             </div>
+
             <div className="form-group">
               <label htmlFor="">Data de inicio</label>
               <input type="date" />
             </div>
+
             <div className="form-group">
               <label htmlFor="">Data de fim</label>
               <input type="date" />
             </div>
+
             <div className="form-submit">
-              <button type="submit">Buscar</button>
+              <button type="submit">
+                Buscar
+                <FaSearch />
+              </button>
             </div>
           </form>
         </main>
