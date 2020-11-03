83543f31cb7bb76c9f10d958eb31152c56a4ea4f
deboranis <dc.nisen@hotmail.com>
2020-11-03 14:51:41 -0300

    "pequenos ajustes na producer home e sobre"

--- src/Pages/Sobre/Sobre.js
+++ src/Pages/Sobre/Sobre.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./sobre.css";
 import CardTime from "../../Components/CardTime/CardTime";
+import Navbar from '../../Components/Navbar/Navbar'
 import time from "../../Components/CardTime/time";
 
 function Sobre() {
   return (
     <div>
+    <Navbar />
       <h3>
         O Elenc.o foi desenvolvido para a Gama Academy em parceria com
         Accenture. Nosso time de desenvolvedores junior é composto por:
