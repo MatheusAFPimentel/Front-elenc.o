85be5b389dfbd535efbc356b0ab9df06692e04fd
Matheus Augusto Ferreira Pimentel <59885528+MatheusAFPimentel@users.noreply.github.com>
2020-11-03 21:14:25 -0300

    Delete Footer.js

--- src/Components/Footer/Footer.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import {FcLike} from "react-icons/fc"
-export default props =>
-    <footer className="footer">
-        <span>
-            Desenvolvido com <FcLike/> pelo grupo Sandy e os Juniors 
-        </span>
-    </footer>
