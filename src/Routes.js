ea8b5edeb7259b13d755043527e29fee1ceb2022
danielbpc2 <danielbpc2@hotmail.com>
2020-11-04 19:11:32 -0300

    fix my mistake

---
+++ src/Routes.js
@@ -0,0 +1,25 @@
+import { Switch, Route } from "react-router-dom";
+
+import Home from "./pages/Home/Home";
+import MeuPerfilAtor from "./pages/MeuPerfilAtor/MeuPerfilAtor";
+import MeuPerfilProdutor from "./pages/MeuPerfilProdutor/MeuPerfilProdutor";
+import ProducerHome from "./pages/ProducerHome";
+import Cadastro from "./pages/cadastro";
+import Sobre from "./pages/Sobre/Sobre";
+import PainelResultados from "./pages/PainelResultados/PainelResultados";
+
+const Routes = () => {
+  return (
+    <Switch>
+      <Route path="/" exact component={Home} />
+      <Route path="/actor/profile" exact component={MeuPerfilAtor} />
+      <Route path="/producer/profile" exact component={MeuPerfilProdutor} />
+      <Route path="/results" exact component={PainelResultados} />
+      <Route path="/dashboard" exact component={ProducerHome} />
+      <Route path="/registro" exact component={Cadastro} />
+      <Route path="/sobre" exact component={Sobre} />
+    </Switch>
+  );
+};
+
+export default Routes;
