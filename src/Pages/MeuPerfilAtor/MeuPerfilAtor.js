989c10a2797b6d982e0b237de902a32450101f68
MatheusAFPimentel <matheus.algustt@gmail.com>
2020-11-10 15:47:26 -0300

    MeuPerfilAtor

--- src/Pages/MeuPerfilAtor/MeuPerfilAtor.js
+++ src/Pages/MeuPerfilAtor/MeuPerfilAtor.js
@@ -1,6 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
+import CardAtores from '../../Components/CardsAtores/CardsAtores'
+import { getActors } from '../../assets/fakedata/api'
+import { Card, CardBody, CardImg, CardText, CardTitle } from "reactstrap";
+
 
 const MeuPerfilAtor = (props) => {
-  return <h1>meu perfil ator</h1>;
+
+  const [actors, setActors] = useState([])
+
+  getActors.then((data) => {
+    setActors(data);
+  });
+
+  return (
+    <div>
+      {actors.map((actor) => <Card key={actor.id} actor={actor} >
+        <CardImg
+          className="img_card"
+          src={actor.avatar}
+          alt="foto do actors"
+        >
+        </CardImg>
+        <CardTitle>
+          <h2>{actor.name}</h2>
+        </CardTitle>
+        <CardText>
+          Idade: {actor.age}
+          <br />
+          Gênero: {actor.sex}
+          <br />
+          DRT: {actor.drt}
+          <br />
+          Atuação: {actor.genre.join(", ")}
+          <br />
+        </CardText>
+        <br />
+        <CardBody>
+          <hr />
+          Suas reservas
+          <hr />
+        </CardBody>
+        <CardText>
+          Você não possui reservas
+        </CardText>
+      </Card>)}
+    </div>
+  )
 };
 export default MeuPerfilAtor;
