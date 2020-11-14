c8ce64427b3a2a21669632aa0105e8327267fe48
danielbpc2 <danielbpc2@hotmail.com>
2020-11-14 17:08:31 -0300

    Perfil Produtor feito

--- src/Pages/MeuPerfilProdutor/MeuPerfilProdutor.js
+++ src/Pages/MeuPerfilProdutor/MeuPerfilProdutor.js
@@ -11,10 +11,24 @@ import "./meuPerfilProdutor.css";
 const MeuPerfilProdutor = (props) => {
   const [user, setUser] = useState({});
   const [reserves, setReserves] = useState([]);
+  // const [stats, setStats] = useState({
+  // mostReservedDate: {},
+  // mostReservedActress: {},
+  // });
 
   useEffect(() => {
     const currentUser = JSON.parse(localStorage.getItem("currentUser"));
     setUser({ ...currentUser });
+    // api
+    //   .get(`/reserve/getMostReservedDatesByProducer/${currentUser.id}`)
+    //   .then((res) => {
+    //     setStats({ mostReservedDate: res.data });
+    //   });
+    // api
+    //   .get(`/reserve/getMostReservedActressesByProducer/${currentUser.id}`)
+    //   .then((res) => {
+    //     setStats({ mostReservedActress: res.data });
+    //   });
     api
       .get(`/reserve/listByProducer/${currentUser.id}`)
       .then((res) => setReserves(res.data));
@@ -24,11 +38,6 @@ const MeuPerfilProdutor = (props) => {
     <div className="perfil_producer_container">
       <div id="producer-pageGrid">
         <Card id="card_producer">
-          <CardImg
-            className="img_card_producer"
-            src="https://placekitten.com/200/300"
-            alt="foto do producer"
-          ></CardImg>
           <div className="card_expand">
             <CardTitle>
               <h2>
@@ -50,35 +59,44 @@ const MeuPerfilProdutor = (props) => {
           </div>
         </Card>
 
-        <div className="reservas-grid-producer">
-          {reserves.length === 0 ? (
-            <CardText>Você não possui reservas</CardText>
-          ) : (
-            reserves.map((reserve, index) => (
-              <Card id="card_reserva" key={reserve.id}>
-                <div className="card_expand">
-                  <CardBody id="card_body">
-                    <header>
-                      <span>
-                        <FcCalendar />
-                        Data reservada:{" "}
-                        {new Date(reserve.reserveDate).toLocaleDateString()}
-                      </span>
-                      <hr />
-                    </header>
-                    <CardText>
-                      <p>Dia Reservado:{reserve.reserveDate}</p>
-                      <p>Nome do producer: {reserve.actress.name}</p>
-                      <p>Cachê do producer: {reserve.actress.price}</p>
-                      <p>Sexo do producer: {reserve.actress.gender}</p>
-                      <p>Gênero de Atuação: {reserve.actress.genre}</p>
-                      <br />
-                    </CardText>
-                  </CardBody>
-                </div>
+        <div className="producer-stat-res-grid">
+          <div className="producer-stats">
+            <div className="stat">
+              <h2>Suas Reservas:</h2>
+            </div>
+          </div>
+          <div className="reservas-grid-producer">
+            {reserves.length === 0 ? (
+              <Card>
+                <CardText>Você não possui reservas</CardText>
               </Card>
-            ))
-          )}
+            ) : (
+              reserves.map((reserve, index) => (
+                <Card id="card_reserva" key={reserve.id}>
+                  <div className="card_expand">
+                    <CardBody id="card_body">
+                      <header>
+                        <span>
+                          <FcCalendar />
+                          Data reservada:{" "}
+                          {new Date(reserve.reserveDate).toLocaleDateString()}
+                        </span>
+                        <hr />
+                      </header>
+                      <CardText>
+                        <p>Dia Reservado:{reserve.reserveDate}</p>
+                        <p>Nome do producer: {reserve.actress.name}</p>
+                        <p>Cachê do producer: {reserve.actress.price}</p>
+                        <p>Sexo do producer: {reserve.actress.gender}</p>
+                        <p>Gênero de Atuação: {reserve.actress.genre}</p>
+                        <br />
+                      </CardText>
+                    </CardBody>
+                  </div>
+                </Card>
+              ))
+            )}
+          </div>
         </div>
       </div>
     </div>
