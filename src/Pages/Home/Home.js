57b1601f71f1af5bcf32d6ab8230fc1244c00a0b
danielbpc2 <danielbpc2@hotmail.com>
2020-11-15 16:11:44 -0300

    Home redireciona se houve login previamente

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import "./home.css";
 import logo from "../../assets/images/logo.svg";
-import { useHistory, Link } from "react-router-dom";
+import { useHistory, Link, Redirect } from "react-router-dom";
 import { findUser } from "../../services/helpers/findUser";
 
 function Home() {
@@ -36,53 +36,60 @@ function Home() {
       setLoginErr(userFound.err);
     }
   };
-
-  return (
-    <div className="home">
-      <img id="home_logo" src={logo} alt="logo elenc.o" />
-      <div className="form_login">
-        <form onSubmit={handleSubmit}>
-          <h2 className="titulo_login">Login</h2>
-          <h3>
-            <span style={{ color: "red" }}>{loginErr}</span>
-          </h3>
-          <label id="label_home">Email</label> <br />
-          <input
-            type="email"
-            placeholder="insira seu email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-            className="input_home"
-          />
-          <br />
-          <label id="label_home">Senha</label>
-          <br />
-          <input
-            type="password"
-            placeholder="insira sua senha"
-            onChange={(e) => setPassword(e.target.value)}
-            value={password}
-            required
-            className="input_home"
-          />
-          <br />
-          <button id="botao_entrar" type="submit">
-            Entrar
-          </button>
-        </form>
-        <div className="container_cadastro">
-          <Link to="/cadastro?role=producer" className="link_cadastro">
-            Cadastre-se como Produtor
-          </Link>
-          <p>ou</p>
-          <Link to="/cadastro?role=actor" className="link_cadastro">
-            Cadastre-se como Elenco
-          </Link>
+  if (JSON.parse(localStorage.getItem("currentUser"))) {
+    if (JSON.parse(localStorage.getItem("currentUser")).role === "producer") {
+      return <Redirect to="/busca" />;
+    } else {
+      return <Redirect to="/actor/profile" />;
+    }
+  } else {
+    return (
+      <div className="home">
+        <img id="home_logo" src={logo} alt="logo elenc.o" />
+        <div className="form_login">
+          <form onSubmit={handleSubmit}>
+            <h2 className="titulo_login">Login</h2>
+            <h3>
+              <span style={{ color: "red" }}>{loginErr}</span>
+            </h3>
+            <label id="label_home">Email</label> <br />
+            <input
+              type="email"
+              placeholder="insira seu email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+              className="input_home"
+            />
+            <br />
+            <label id="label_home">Senha</label>
+            <br />
+            <input
+              type="password"
+              placeholder="insira sua senha"
+              onChange={(e) => setPassword(e.target.value)}
+              value={password}
+              required
+              className="input_home"
+            />
+            <br />
+            <button id="botao_entrar" type="submit">
+              Entrar
+            </button>
+          </form>
+          <div className="container_cadastro">
+            <Link to="/cadastro?role=producer" className="link_cadastro">
+              Cadastre-se como Produtor
+            </Link>
+            <p>ou</p>
+            <Link to="/cadastro?role=actor" className="link_cadastro">
+              Cadastre-se como Elenco
+            </Link>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default Home;
